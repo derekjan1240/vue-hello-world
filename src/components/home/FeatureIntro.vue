0d5a31fa8763c0275ebb1627beabe450a546ae44
Kai <derekjan1240@gmail.com>
2021-10-31 15:30:28 +0800

    首頁畫面設定

---
+++ src/components/home/FeatureIntro.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>
+    {{ title }}
+  </h1>
+  <p>{{ intro }}</p>
+  <button class="btn" @click="$router.push({ path: link })">前往</button>
+</template>
+
+<script>
+export default {
+  name: "FeatureIntro",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "我是功能標題",
+    },
+    intro: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    link: {
+      type: String,
+      required: true,
+      default: "/",
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn {
+  width: 200px;
+  height: 50px;
+  border: 0;
+  border-radius: 5px;
+  background: cornsilk;
+  font-size: 1.25rem;
+  font-weight: 800;
+  letter-spacing: 1.5px;
+  cursor: pointer;
+  margin: 1em 0;
+}
+
+.btn:hover {
+  background: #f1e8b8;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- <div class="home"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- </div> -->
+  <div>
+    <div class="scroll-area">
+      <h1>
+        Project intro ...
+      </h1>
+    </div>
+    <div class="scroll-area">
+      <FeatureIntro
+        title="Feature 01 - Todo List"
+        intro="就是基本的 Todo List"
+        link="/todo"
+      />
+    </div>
+    <div class="scroll-area">
+      <FeatureIntro
+        title="Feature 02 - User Post"
+        intro="就是基本的 User Post"
+        link="/featureB"
+      />
+    </div>
+    <div class="scroll-area">
+      <FeatureIntro title="Feature 03" intro="還沒想到" link="/featureC" />
+    </div>
+  </div>
+</template>
+
+<script>
+import FeatureIntro from "@/components/home/FeatureIntro.vue";
+
+export default {
+  name: "Home",
+  components: {
+    FeatureIntro,
+  },
+};
+</script>
+
+<style scoped>
+.scroll-area {
+  width: 100%;
+  height: 100vh;
+  font-size: 60px;
+}
+
+.scroll-area {
+  scroll-snap-align: start;
+}
+
+.scroll-area {
+  margin: 0 auto;
+}
+
+.scroll-area {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+
+.scroll-area:nth-of-type(1) {
+  background: #49b293;
+}
+
+.scroll-area:nth-of-type(2) {
+  background: #c94e4b;
+}
+
+.scroll-area:nth-of-type(3) {
+  background: #4cc1be;
+}
+
+.scroll-area:nth-of-type(4) {
+  background: #8360a6;
+}
+</style>
