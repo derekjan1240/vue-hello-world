3629c44ceba18b475ea22653e3a87b3615432c46
Kai <derekjan1240@gmail.com>
2021-10-31 15:27:14 +0800

    新增 404 頁面

---
+++ src/views/Page404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="image-container">
+    <img src="@/assets/images/NotFound.png" />
+  </div>
+</template>
+
+<style scoped>
+.image-container {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: -1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
