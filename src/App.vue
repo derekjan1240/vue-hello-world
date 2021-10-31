a174cd211d8a190caf9d84d73f27769c602bb937
Kai <derekjan1240@gmail.com>
2021-10-31 15:29:17 +0800

    新增 Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from "@/components/Navbar.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Noto Sans TC", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  overflow: auto;
+  scroll-snap-type: y mandatory;
+  max-height: 100vh;
+  font-size: 1rem;
+  letter-spacing: 1.25px;
+  min-height: 100vh;
+  background: #e6ebe0;
+}
+
+.container {
+  scroll-snap-align: start;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="nav">
+    <div id="navAvatarWrapper">
+      <img
+        id="navAvatar"
+        src="@/assets/images/Home.png"
+        @click="$router.push({ path: '/' })"
+      />
+    </div>
+    <div id="navButtonWrapper">
+      <router-link to="/todo" class="nav-btn">
+        Todo
+      </router-link>
+      <router-link to="/featureB" class="nav-btn">
+        Posts
+      </router-link>
+      <router-link to="/featureC" class="nav-btn">
+        功能 C
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+#nav {
+  scroll-snap-align: start;
+  padding: 10px;
+  font-size: 1.5em;
+  display: flex;
+  background: #fff;
+}
+
+#nav a {
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  background: #d7fcd4;
+}
+
+#navAvatarWrapper {
+  width: 80px;
+  display: flex;
+  align-items: center;
+}
+
+#navAvatar {
+  max-width: 100%;
+  cursor: pointer;
+}
+
+#navButtonWrapper {
+  flex-grow: 1;
+  display: flex;
+  justify-content: end;
+}
+
+.nav-btn {
+  width: 150px;
+  padding: 0.8em;
+  margin: 1em;
+  border: 0;
+  border-radius: 5px;
+  background: #fff;
+  color: #2c3e50;
+  font-size: 1.25rem;
+  letter-spacing: 1.5px;
+}
+
+.nav-btn:hover {
+  background: #d7fcd4;
+}
+</style>
